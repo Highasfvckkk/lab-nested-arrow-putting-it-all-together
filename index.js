baf79e8887bca4136291ab0067fea7585a268342
Kinoti <vickykinoti001@gmail.com>
2025-12-06 16:16:45 +0300

    Final Commit

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+let userInfo = {
+  username: "User1",
+  password: "password123",
+};
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
 
-
+  return (passwordAttempt) => {
+    attemptCount++;
+    if (passwordAttempt === userInfo.password && attemptCount <= 3) {
+      return "Login successful";
+    } else if (passwordAttempt !== userInfo.password && attemptCount <= 3) {
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+    if (attemptCount > 3) {
+      return `Account locked due to too many failed login attempts`;
+    }
+  };
+}
+createLoginTracker();
 
 module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+  ...(typeof createLoginTracker !== "undefined" && { createLoginTracker }),
 };
